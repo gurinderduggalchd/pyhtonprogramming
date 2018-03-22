165711433ce85355227358520d6cba7fbfc43522
Gurinder Duggal <gurinderduggalchd@gmail.com>
2018-03-22 17:21:35 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  6 10:36:07 2017
+
+@author: duggal
+"""
+
+no1 =  int(input('enter first number'))
+no2 = int(input('enter second number'))
+
+sum = no1 + no2
+
+print('sum is ')
+print(sum)
+
