bb15de7964d2ef65b112e12c5c6c7ffaae20b5c2
Gurinder Duggal <gurinderduggalchd@gmail.com>
2018-03-23 13:41:14 +0530

    Week 5 Task 5 and 6

---
+++ week5-task5and6.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 13:31:12 2018
+
+@author: 
+"""
+
+num = int(input("Enter a integer number : "))
+
+rem = num % 7
+fits = num // 7
+print("Remainder after dividing by 7 is :", rem)
+print("number 7 fits is : ", fits, " times")
