571fb4a6bc295035f152f912a10658659cbedb95
Gurinder Duggal <gurinderduggalchd@gmail.com>
2018-03-23 12:37:53 +0530

    Week 4 Task 5 and 8

---
+++ week4-task5.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 11:45:14 2018
+
+@author: duggal
+"""
+
+x = 19.93
+y = 20.0
+z = y–x
+print(z)
---
+++ week4-task8.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 12:21:51 2018
+
+@author: duggal
+"""
+
+name = input("enter your name : ")
+age = int(input("enter your age : "))
+
+print("Hello ", name, "next year you will be ", age+1, " years old!")
