c871633ec79312f19af81642e2013e9cce505252
Gurinder Duggal <gurinderduggalchd@gmail.com>
2018-03-23 10:19:47 +0530

    Add files via upload

---
+++ week2-task5.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 10:00:48 2018
+
+@author:
+"""
+
+username = input("Hi there, whats your name ? ")  # input is used to take input from keyboard
+print("Hello")
+print(username)
+print("How are you ?")
+
+
+
