d9248c9649aa4f3949c94520d0e80516d692495b
Gurinder Duggal <gurinderduggalchd@gmail.com>
2018-03-23 10:02:49 +0530

    Week 2 - task 4
    This program show the use of print command

---
+++ week2-task4.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 09:49:08 2018
+
+@author:
+"""
+
+print("Hello World")     # print command is used to give output on the screen          
+print("I am in my ISD class right now")
+
