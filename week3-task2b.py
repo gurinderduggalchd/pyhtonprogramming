df76d95596efe41291a4dfa43d9b65c0f4566dff
Gurinder Duggal <gurinderduggalchd@gmail.com>
2018-03-23 11:34:16 +0530

    Week 3 Task 2b

---
+++ week3-task2b.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 11:30:53 2018
+
+@author: 
+"""
+
+print(2 / 3)     # / is used for normal division 
+print(2 // 3)    # // is used for integer division. 
+
