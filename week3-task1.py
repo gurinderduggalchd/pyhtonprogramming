fc8e9ad0ce93009aa0003b44abd7df3a51373e3d
Gurinder Duggal <gurinderduggalchd@gmail.com>
2018-03-23 11:11:40 +0530

    Week 3 Task 1
    Program to input two numbers and add them to find their sum

---
+++ week3-task1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 10:19:19 2018
+
+@author: 
+    
+"""
+
+num1 = int(input("Enter First number : "))
+num2 = int(input("Enter Second number : "))
+
+sum = num1 + num2
+
+print("sum is : " + str(sum))   # str() function is used to convert the sum variable to string type
+
+
